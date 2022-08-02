06f128844f62a555698f9e3ced0255178aa8be77
Nguyễn Thiện Nhân <thiennhan.copyright@gmail.com>
2022-08-02 23:55:40 +0700

    Update

--- public/image/china.js
+++ public/image/china.js
@@ -1,11 +1,11 @@
-exports.name = '/images/sex';
+exports.name = '/images/china';
 exports.index = async(req, res, next) => {
     try {
-        const sex = require('./data/json/sex.json');
-        var image = sex[Math.floor(Math.random() * sex.length)].trim();
+        const china = require('./data/json/china.json');
+        var image = sex[Math.floor(Math.random() * china.length)].trim();
         res.jsonp({
             url: image,
-            count: sex.length,
+            count: china.length,
             author: 'tnhan'
         });
     } catch (e) {
