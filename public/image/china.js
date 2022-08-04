6a6760c979ca48c555a0be6f5ae7a7849fa82810
Nguyễn Thiện Nhân <thiennhan.copyright@gmail.com>
2022-08-04 22:35:41 +0700

    Update China

--- public/image/china.js
+++ public/image/china.js
@@ -3,9 +3,12 @@ exports.index = async(req, res, next) => {
     try {
         const china = require('./data/json/china.json');
         var image = china[Math.floor(Math.random() * china.length)].trim();
+        const cadao = require('./data/json/cadao.json');
+        var fast1 = cadao[Math.floor(Math.random() * cadao.length)].trim();
         res.jsonp({
             url: image,
             count: china.length,
+            fast: fast1,
             author: 'tnhan'
         });
     } catch (e) {
